39d7bafb1dc5751e7f15189d7103878ea78286be
aldealexandre <alde.alexandre@hotmail.com>
2025-07-10 19:56:49 -0300

    Verbos GET, POST, PUT e DELETE na programs

--- BackendApi/Program.cs
+++ BackendApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Components.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,31 +18,70 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+
+List<Produto> produtos = new List<Produto>()
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Produto() {Id = 1, Nome = "Mouse sem fio", Preco = 99.90M, Estoque = 50},
+    new Produto() {Id = 2, Nome = "Teclado", Preco = 49.90M, Estoque = 30}
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/produtos", () => 
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    return produtos;
+});
 
-app.MapGet("/test", () => "Endpoint de teste!");
+app.MapGet("/produtos/{id}", (int id) => 
+{ 
+    var produto = produtos.FirstOrDefault(x => x.Id == id);
+    return produto is not null
+    ? Results.Ok(produto)
+    : Results.NotFound($"Produto com ID {id} não encontrado.");
+});
+
+app.MapPost("/produtos", (Produto novoProduto) => 
+{ 
+    produtos.Add(novoProduto);
+    return Results.Created();
+});
+
+app.MapPut("/produtos/{id}", (int id, Produto produtoAtualizado) => 
+{
+    var produto = produtos.FirstOrDefault(x => x.Id == id);
+    if (produto is null)
+    {
+        return Results.NotFound($"Produto com ID {id} não encontrado.");
+    }
+
+    produto.Nome = produtoAtualizado.Nome;
+    produto.Preco = produtoAtualizado.Preco;
+    produto.Estoque = produtoAtualizado.Estoque;
+
+    return Results.Ok(produto);
+
+});
+
+app.MapDelete("produtos/{id}", (int id) => 
+{
+    var produto = produtos.FirstOrDefault(x => x.Id == id);
+    if (produto is null)
+    {
+        return Results.NotFound($"Produto com ID {id} não encontrado.");
+    }
+
+    produtos.Remove(produto);
+    return Results.NoContent();
+
+});
+
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+
+class Produto
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; }
+
+} 
